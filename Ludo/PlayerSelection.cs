2e98151cd1b72c967c0e0d716caa96ab17b901dc
Serjinho11 <sergiu.linte@ulbsibiu.ro>
2020-12-18 15:42:19 +0200

    Player selection and start popping off.

--- Ludo/PlayerSelection.cs
+++ Ludo/PlayerSelection.cs
@@ -12,14 +12,71 @@ namespace Ludo
 {
     public partial class PlayerSelection : Form
     {
+        int playerSelectat = 0;
+
+
+
         public PlayerSelection()
         {
             InitializeComponent();
+
+
+            
+
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                playerSelectat++;
+            }
+            if (playerSelectat >= 2)
+            {
+                btnStart.Visible = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                playerSelectat++;
+            }
+            if (playerSelectat >= 2)
+            {
+                btnStart.Visible = true;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+            {
+                playerSelectat++;
+            }
+            if (playerSelectat >= 2)
+            {
+                btnStart.Visible = true;
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBox4.Checked == true)
+            {
+                playerSelectat++;
+            }
+            if (playerSelectat >= 2)
+            {
+                btnStart.Visible = true;
+            }
         }
     }
 }
